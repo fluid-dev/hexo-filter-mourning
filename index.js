3bb4f356ed22d8ee627f4ff6da6b31540b13cbe2
zkqiang <zkqiang@126.com>
2020-04-04 13:35:39 +0800

    Support for IE

--- index.js
+++ index.js
@@ -10,17 +10,34 @@ hexo.extend.filter.register('after_render:html', function (html) {
     return;
   }
 
+  const css = 'html { \
+    -webkit-filter: grayscale(100%); /* webkit */ \
+    -moz-filter: grayscale(100%); /* firefox */ \
+    -ms-filter: grayscale(100%); /* ie9 */ \
+    -o-filter: grayscale(100%); /* opera */ \
+    filter: grayscale(100%); \
+    filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1); filter:gray; /* ie9- */ \
+  }';
+
   const js = `
   <script type="text/javascript">
-    var now = new Date();
-    var dayStr = '${ config.memorial_day.day }';
-    var day = dayStr ? new Date(dayStr) : new Date();
-    var isMemorialDay = now.getFullYear() === day.getFullYear() && now.getMonth() === day.getMonth() && now.getDay()  === day.getDay();
-    if (isMemorialDay) {
-      var html = document.getElementsByTagName('html')[0];
-      html.style.setProperty('-webkit-filter', 'grayscale(100%)');
-      html.style.setProperty('filter', 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)')
-    }
+    (function() {
+      var now = new Date();
+      var dayStr = '${ config.memorial_day.day }';
+      var day = dayStr ? new Date(dayStr) : new Date();
+      var isMemorialDay = now.getFullYear() === day.getFullYear() && now.getMonth() === day.getMonth() && now.getDay()  === day.getDay();
+      if (isMemorialDay) {
+        if (document.all) {
+          window.style = '${ css }';
+          document.createStyleSheet('javascript:style');
+        } else {
+          var style = document.createElement('style');
+          style.type = 'text/css';
+          style.innerHTML = '${ css }';
+          document.getElementsByTagName('HEAD').item(0).appendChild(style);
+        }
+      }
+    })();
   </script>
   `;
 
